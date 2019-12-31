fcad0ebc7cfb98509073592bb82b37ee420221a8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:43:36 -0600

    Rewriting as Laminas Project package

---
+++ src/LaminasRbacAssertionInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @see       https://github.com/mezzio/mezzio-authorization-rbac for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-authorization-rbac/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-authorization-rbac/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Mezzio\Authorization\Rbac;
+
+use Laminas\Permissions\Rbac\AssertionInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+interface LaminasRbacAssertionInterface extends AssertionInterface
+{
+    public function setRequest(ServerRequestInterface $request) : void;
+}
