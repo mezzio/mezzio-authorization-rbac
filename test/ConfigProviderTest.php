1d10caca25dc304398e126f647f971b8499f6b62
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:01:54 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/ConfigProviderTest.php
+++ test/ConfigProviderTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-authorization-rbac for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-authorization-rbac/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-authorization-rbac/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace MezzioTest\Authorization\Rbac;
